6d16de3669afed17e2e2ad24fb7ac956d0e646dc
AndriyCO <Aolijnyk73@gmail.com>
2017-07-20 09:40:25 +0300

    nothing

--- Project_forHomeWork/src/ua/Main.java
+++ Project_forHomeWork/src/ua/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello!"+"exersise N 1");
-
+		System.out.println("perhaps will bee written something htere");
 	}
 
 }
